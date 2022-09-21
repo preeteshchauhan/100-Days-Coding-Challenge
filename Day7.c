dddd448ec0a8fe153c4d09d69149baa28ce9624e
Preetesh Chauhan <110602313+preeteshchauhan@users.noreply.github.com>
2022-09-21 20:27:16 +0530

    day7
    Given an array, of size , reverse it.
    
    Example: If array, , after reversing it, the array should be, .
    
    Input Format
    
    The first line contains an integer, , denoting the size of the array. The next line contains  space-separated integers denoting the elements of the array.
    
    Constraints
    
    
    , where  is the  element of the array.
    
    Output Format
    
    The output is handled by the code given in the editor, which would print the array.

---
+++ Day7.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,i,s,temp;
+    scanf("%d",&n);
+    int a[n];
+  
+   for(i=0;i<n;i++){
+          scanf("%d ",&a[i]);
+   }
+   for(i=0;i<n/2;i++){
+       temp=a[i];
+       a[i]=a[n-i-1];
+       a[n-i-1]=temp;
+   }
+   for(i=0;i<n;i++){
+       printf("%d ",a[i]);
+   }
+}
